04b6a36a7265ba7b0b59b861b473fdfb10bc1d20
tldjfj123 <dksckd0804@naver.com>
2025-11-02 21:04:55 +0900

    feat : WinningLotto 클래스 구현 완료

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,25 @@
 package lotto.domain;
 
 public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, int bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto winningNumbers, int bonusNumber) {
+        if (winningNumbers.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Rank determineRank(Lotto userLotto) {
+        int matchCount = userLotto.countMatchingNumbers(winningNumbers);
+        boolean matchBonus = userLotto.contains(bonusNumber);
+
+        return Rank.valueOf(matchCount, matchBonus);
+    }
 }
