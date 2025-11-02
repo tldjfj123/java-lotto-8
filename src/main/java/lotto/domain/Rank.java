dcfaa3b5fef49fa1e0471db81444e6924312fe5c
tldjfj123 <dksckd0804@naver.com>
2025-11-02 20:57:51 +0900

    feat : Rank 클래스 구현 완료

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,4 +1,50 @@
 package lotto.domain;
 
 public enum Rank {
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
+
+    private final int matchCount;
+    private final int prizeMoney;
+
+    Rank(int matchCount, int prizeMoney) {
+        this.matchCount = matchCount;
+        this.prizeMoney = prizeMoney;
+    }
+
+    public int getPrizeMoney() {
+        return prizeMoney;
+    }
+
+    public int getMatchCount() {
+        return matchCount;
+    }
+
+    public static Rank valueOf(int matchCount, boolean matchBonus) {
+        if(matchCount == FIRST.getMatchCount()) {
+            return FIRST;
+        }
+
+        if (matchCount == 5 && matchBonus) {
+            return SECOND;
+        }
+
+        if (matchCount == 5) {
+            return THIRD;
+        }
+
+        if (matchCount == 4) {
+            return FOURTH;
+        }
+
+        if (matchCount == 3) {
+            return FIFTH;
+        }
+
+        return MISS;
+    }
 }
